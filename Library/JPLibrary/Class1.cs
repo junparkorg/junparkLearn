167d887b08347766dd863e752ba28ecd37e71644
junpark12 <ante5273@hotmail.com>
2021-09-02 16:07:24 +0900

    ok

--- Library/JPLibrary/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace JPLibrary
-{
-    public class Class1
-    {
-    }
-}
---
+++ Library/JPLibrary/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JPLibrary
+{
+    public class Util
+    {
+
+        public string Version
+        {
+            get
+            {
+                return "1.1.1";
+            }
+        }
+    }
+}
