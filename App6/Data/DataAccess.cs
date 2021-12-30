6167cb3d3439e3e103c8f0116f761c743c00fcef
junpark12 <ante5273@hotmail.com>
2021-12-30 14:43:25 +0900

    dd

---
+++ App6/Data/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace App6.Data
+{
+    public class DataAccess
+    {
+        // test for code Security.
+        private const string DataAccessKey = "server=1.1.1.1;uid=sa;pwd=NotAPassword;database=nodb";
+
+        
+        public async Task<SqlConnection> CreateConnection()
+        {
+            SqlConnection conn = new SqlConnection(DataAccessKey);
+            await conn.OpenAsync();
+            return conn;
+        }
+    }
+}
