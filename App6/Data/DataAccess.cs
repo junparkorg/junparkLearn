42af023376c5f2cbc0a1f42029e3d97fc928369a
junpark12 <ante5273@hotmail.com>
2022-01-03 15:54:35 +0900

    clear dataaccess logic

--- App6/Data/DataAccess.cs
+++ App6/Data/DataAccess.cs
@@ -6,14 +6,14 @@ namespace App6.Data
     public class DataAccess
     {
         // test for code Security.
-        private const string DataAccessKey = "server=1.1.1.1;uid=sa;pwd=NotAPassword;database=nodb";
+        //private const string DataAccessKey = "server=1.1.1.1;uid=sa;pwd=NotAPassword;database=nodb";
 
         
-        public async Task<SqlConnection> CreateConnection()
-        {
-            SqlConnection conn = new SqlConnection(DataAccessKey);
-            await conn.OpenAsync();
-            return conn;
-        }
+        //public async Task<SqlConnection> CreateConnection()
+        //{
+        //    SqlConnection conn = new SqlConnection(DataAccessKey);
+        //    await conn.OpenAsync();
+        //    return conn;
+        //}
     }
 }
