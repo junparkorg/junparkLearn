1f44032267287bfefef0d8f06dafe18ba46094e0
junpark12 <ante5273@hotmail.com>
2022-01-12 13:48:10 +0900

    added build time info!!

---
+++ App6/Util/BuildInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace App6.Util
+{
+    public class BuildInfo
+    {
+		public static DateTime GetLinkerTime(Assembly assembly)
+		{
+			const string BuildVersionMetadataPrefix = "+build";
+
+			var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+			if (attribute?.InformationalVersion != null)
+			{
+				var value = attribute.InformationalVersion;
+				var index = value.IndexOf(BuildVersionMetadataPrefix);
+				if (index > 0)
+				{
+					value = value[(index + BuildVersionMetadataPrefix.Length)..];
+					return DateTime.ParseExact(value, "yyyy-MM-ddTHH:mm:ss:fffZ", CultureInfo.InvariantCulture);
+				}
+			}
+
+			return default;
+		}
+
+	}
+}
