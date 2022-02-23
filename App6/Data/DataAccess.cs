6032be34ae4c323fb6e5b403cc364099b0da0edd
junpark12 <ante5273@hotmail.com>
2022-02-23 17:25:38 +0900

    asdf

--- App6/Data/DataAccess.cs
+++ App6/Data/DataAccess.cs
@@ -6,7 +6,7 @@ namespace App6.Data
     public class DataAccess
     {
         // test for code Security.
-        private const string DataAccessKey = "server=1.1.1.1;uid=sa;pwd=NotAPassword;database=nodb";
+        private const string DataAccessKey = "server=211.11.12.13;uid=sa;pwd=NotAPassword;database=nodb";
 
         
         public async Task<SqlConnection> CreateConnection()
